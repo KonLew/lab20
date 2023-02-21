39a759d17c4853c21bb292cce7694d3a67528a31
KonLew <surangrat231@gmail.com>
2023-02-21 17:15:44 +0700

    lab20_2

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,16 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+    double dotx = max(R1->x,R2->x);
+    double doty = min(R1->y,R2->y);
 
+    double maxy = max((R1->y-R1->h),(R2->y-R2->h));
+    double maxx = min((R1->x+R1->w),(R2->x+R2->w));
+
+    double z = (maxx-dotx)*(doty-maxy);
+    if(z > 0) return z;
+    else return 0;
 
 
 }
